4d41692610eb665354db9104029d1e75db5eac0d
Gundla <Sampath.Gundla@aig.com>
2020-02-14 12:25:57 -0600

    add code

--- jobs/orgJobs.groovy
+++ jobs/orgJobs.groovy
@@ -24,21 +24,26 @@ organizationFolder("/sgundla/git-org-scanner") {
 
     configure {
       def traits = it / navigators / 'org.jenkinsci.plugins.github__branch__source.GitHubSCMNavigator' / traits
-      traits << 'org.jenkinsci.plugins.github_branch_source.BranchDiscoveryTrait' {
-          strategyId 1
+      traits << 'jenkins.scm.impl.trait.RegexSCMSourceFilterTrait plugin="scm-api@2.6.3"' {
+          regex '.*'
       }
-      traits << 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait' {
-          strategyId 2
-          trust(class: 'org.jenkinsci.plugins.github_branch_source.ForkPullRequestDiscoveryTrait$TrustEveryone')
+      traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+          strategyId 3
       }
-      traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
-          strategyId 2
+      traits << 'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait plugin="scm-api@2.6.3"' {
+          includes '*',
+          excludes 'master'
       }
     }
     projectFactories {
       workflowMultiBranchProjectFactory {
         // Relative location within the checkout of your Pipeline script.
         scriptPath("Jenkinsfile")
+      }
+    }
+    properties {
+      noTriggerOrganizationFolderProperty {
+        branches('feature.*')
       }
     }
 }
